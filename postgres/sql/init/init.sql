991ebff258f22122f4e63da33c69422faad98472
Kensora <diligul.alex@gmail.com>
2022-01-29 17:09:10 +0300

    Init script fix

--- postgres/sql/init/init.sql
+++ postgres/sql/init/init.sql
@@ -1,26 +1,25 @@
 DO $$
 BEGIN
-
     IF NOT EXISTS(
         SELECT schema_name
           FROM information_schema.schemata
           WHERE schema_name = 'guard'
-      )
+    )
     THEN
       EXECUTE 'CREATE SCHEMA guard';
     END IF;
-
 END
 $$;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TYPE guard.user_status AS ENUM ('ACTIVE', 'DELETED');
+CREATE TYPE guard.user_status AS ENUM ('ACTIVE', 'DELETED', 'PENDING');
 
 CREATE TABLE IF NOT EXISTS guard.user (
   id uuid NOT NULL DEFAULT uuid_generate_v4(),
   login text NOT NULL,
   password text NOT NULL,
-  status guard.user_status NOT NULL DEFAULT 'ACTIVE',
+  status guard.user_status NOT NULL DEFAULT 'PENDING',
+
   PRIMARY KEY(id),
   UNIQUE(login)
 );
